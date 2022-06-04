3c08554aa96684eaf2750b9f403120a999fcebee
Donatus <donatus234@gmail.com>
2022-06-04 12:32:11 +0100

    modify some css property

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ document.getElementById("num2-el").textContent=num2
 let sumEl= document.getElementById("sum-el")
 function add(){
     let result = num1 + num2
-    sumEl.textContent = "Sum:" + result
+    sumEl.textContent = "=" + result
 }
 function subtract(){
     let result = num1 - num2
-    sumEl.textContent = "Sum:" + result
+    sumEl.textContent = "=" + result
 }
 function multiply(){
     result = num1 * num2
-    sumEl.textContent = "Sum:" + result
+    sumEl.textContent = "=" + result
 }
 function divide(){
     result = num1 / num2
-    sumEl.textContent = "Sum:"+ result
+    sumEl.textContent = "="+ result
 }
